0b69562371c6692352d225fed3861f098b1c9e6d
devskull-beep <tucorreo@gmail.com>
2026-08-13 05:13:07 +0200

    Update 2026-08-13_05:13

--- app/src/main/java/com/librecuts/plus/MainActivity.kt
+++ app/src/main/java/com/librecuts/plus/MainActivity.kt
@@ -2,14 +2,10 @@ package com.librecuts.plus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.librecuts.plus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
